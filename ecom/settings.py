00e722c582734b83e496d053ea413c6074eb24bb
suhani singh <suhanisingh232005@gmail.com>
2026-04-13 10:35:29 +0530

    Host Added

--- ecom/settings.py
+++ ecom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i$&2_)s=wz8#rn)6ikd7von#c6r+$pijt_58r8ib#u05_(9u83
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
